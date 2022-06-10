20dfa8861cd67265f24d1e52c768489ad1c289dd
VICTOR FREITAS SILVA <victor.fsilva45@gmail.com>
2022-06-10 11:35:08 -0300

    form js

--- aula-7-forms/form.js
+++ aula-7-forms/form.js
@@ -1,4 +1,15 @@
 function dados(){
-    let nome = nome(document.getElementsByName (nome).value)
-    alert (document.getElementsByName)
+    let inputnome = document.getElementById('nome')
+    let inputcpf = document.getElementById('cpf')
+    let inputemail = document.getElementById('email')
+    let inputinstrumento = document.getElementById ('instrumento')
+
+    let obj = new Object()
+    obj.nome = inputnome.value
+    obj.cpf = inputcpf.value
+    obj.email = inputemail.value
+    obj.instrumento = inputinstrumento.value
+
+    let objeto = JSON.stringify(obj)
+    alert(objeto)
 }
